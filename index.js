eb2f8a53be12c4b39d0da75943c84461d950a084
vibhoooo <cse21105@iiitkalyani.ac.in>
2024-06-20 02:03:52 +0530

    updated the entrypoint

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { init } = require("./socket");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/dbConnection");
+connectDB();
+const errorHandler = require("./middlewares/errorHandler");
+const port = process.env.PORT;
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+app.use("/users", require("./routes/userRoutes"));
+app.use("/lobbies", require("./routes/lobbyRoutes"));
+app.use(errorHandler);
+const io = init(server);
+io.on("connection", (socket) => {
+	console.log("New client connected");
+	socket.on("disconnect", () => {
+		console.log("Client disconnected");
+	});
+});
+server.listen(port, () => {
+	console.log(`Server running on port ${port}`);
+});
