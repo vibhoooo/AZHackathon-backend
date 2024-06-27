b4e765a7880e0de2cd830bcd6af1311c5923bf08
vibhoooo <cse21105@iiitkalyani.ac.in>
2024-06-28 02:51:16 +0530

    mcq routes refactored

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,10 +4,6 @@ const validateTokenHandlerUser = require("../middlewares/validateTokenHandlerUse
 const { signupUser } = require("../controllers/userControllers");
 const { loginUser } = require("../controllers/userControllers");
 const { logoutUser } = require("../controllers/userControllers");
-const { createMcq } = require("../controllers/userControllers");
-const { readMcq } = require("../controllers/userControllers");
-const { updateMcq } = require("../controllers/userControllers");
-const { deleteMcq } = require("../controllers/userControllers");
 router.route(
 	"/signup"
 ).post(
@@ -22,29 +18,5 @@ router.route(
 	"/logout"
 ).post(
 	logoutUser
-);
-router.route(
-	"/createMcq"
-).post(
-	validateTokenHandlerUser,
-	createMcq
-);
-router.route(
-	"/readMcq"
-).post(
-	validateTokenHandlerUser,
-	readMcq
-);
-router.route(
-	"/updateMcq/:id"
-).post(
-	validateTokenHandlerUser,
-	updateMcq
-);
-router.route(
-	"/deleteMcq/:id"
-).post(
-	validateTokenHandlerUser,
-	deleteMcq
 );
 module.exports = router;
