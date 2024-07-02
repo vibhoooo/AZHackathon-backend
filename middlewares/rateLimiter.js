474021f06f6264b4633d9376cf15e9c864904759
vibhoooo <cse21105@iiitkalyani.ac.in>
2024-07-03 02:42:06 +0530

    modified rate limiter

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,7 +1,7 @@
 const rateLimit = require('express-rate-limit');
 
 const apiLimiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
+	windowMs: 1 * 60 * 1000, // 1 minutes
 	max: 100, // Limit each IP to 100 requests per windowMs
 	message: 'Too many requests, please try again later.'
 });
