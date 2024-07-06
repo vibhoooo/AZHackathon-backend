208cfc7220232b634f3f9ea84bed60c418196a8e
vibhoooo <cse21105@iiitkalyani.ac.in>
2024-07-06 12:46:20 +0530

    used redis for rate limiting

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,9 +1,42 @@
-const rateLimit = require('express-rate-limit');
-
-const apiLimiter = rateLimit({
-	windowMs: 1 * 60 * 1000, // 1 minutes
-	max: 100, // Limit each IP to 100 requests per windowMs
-	message: 'Too many requests, please try again later.'
-});
+const asyncHandler = require("express-async-handler");
+const Redis = require("ioredis")
+const moment = require("moment")
+const redisClient = new Redis({ url: "redis://localhost:6379" })
+const RATELIMIT_DURATION_IN_SECONDS = 60
+const NUMBER_OF_REQUEST_ALLOWED = 5
+const apiLimiter = asyncHandler(
+	async (req, res, next) => {
+		const userId = req.headers["user_id"]
+		const currentTime = moment().unix()
+		const result = await redisClient.hgetall(userId)
+		if (Object.keys(result).length === 0) {
+			await redisClient.hset(userId, {
+				"createdAt": currentTime,
+				"count": 1
+			})
+			return next()
+		} 
+		if (result) {
+			if (diff > RATELIMIT_DURATION_IN_SECONDS) {
+				await redisClient.hset(userId, {
+					"createdAt": currentTime,
+					"count": 1
+				})
+				return next()
+			}
+		}
+		if (result["count"] >= NUMBER_OF_REQUEST_ALLOWED) {
+			return res.status(429).json({
+				"success": false,
+				"message": "user-ratelimited"
+			})
+		} else {
+			await redisClient.hset(userId, {
+				"count": parseInt(result["count"]) + 1
+			})
+			return next()
+		}
+	}
+);
 
 module.exports = apiLimiter;
